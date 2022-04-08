bb81422dd28a4dabdf7a519cc44b35e3b3f3ba68
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2022-04-08 12:24:52 +0200

    adding register component

---
+++ src/components/RegisterUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <input type="text" name="name" />
+    <input type="email" name="email" />
+    <input type="password" name="password" />
+    <input type="password" name="password_confirm" />
+    <input type="submit" value="Zarejestruj się" />
+  </form>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped></style>
