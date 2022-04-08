069fb19f11a5ef612dc007acae2f4aa734647b12
Krzysztof Cywiński <krzysztof.cywinski@quantumlab.co>
2022-04-08 12:47:29 +0200

    Added EditView and a scaffolding of CategoryManager

---
+++ src/components/CategoryManager.vue
@@ -0,0 +1,29 @@
+<script setup>
+import IconTrash from '@/components/icons/IconTrash.vue'
+import IconAdd from '@/components/icons/IconAdd.vue'
+defineProps({
+  categories: {
+    type: Array,
+    required: false,
+    default: [
+        { name: "One" },
+        { name: "Two" }
+    ]
+  }
+})
+</script>
+
+<template>
+  <div class="categories">
+    <h1 class="green">Categories</h1>
+    <div v-for="category in categories" :key="category.name">
+        <span>{{ category.name }}</span><IconTrash />
+    </div>
+    <div id="new-category-container">
+        <input type="text" id="new-category-input"/><IconAdd />
+    </div>    
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/EditView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CategoryManager from '../components/CategoryManager.vue';
+</script>
+
+<template>
+  <main>
+    <CategoryManager />
+  </main>
+</template>
