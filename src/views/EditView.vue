469f09cbcc59727049dd7626e3a256d0bce4fc57
Krzysztof Cywiński <krzysztof.cywinski@quantumlab.co>
2022-04-08 15:23:19 +0200

    Convert EditView from script setup to plain script

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,45 +1,48 @@
-<script setup>
+<script>
 import CategoryManager from '../components/CategoryManager.vue'
 import { ref } from 'vue'
 
-function addCategory(name) {
-    let newCat = {id: nextId++, name: name};
-    categories.value.push(newCat);
-}
-
-function _getCategoryIdx(id) {
-    let idx = -1;
-    for (let i = 0; i < categories.value.length; i++) {
-        if (categories.value[i].id === id) {
-            idx = i;
-            break;
+export default {
+    data() {
+        return {
+            nextId: 4,
+            categories: [
+                { id: 1, name: "One" },
+                { id: 2, name: "Two" },
+                { id: 3, name: "Three" }
+            ],
         }
-    }
-    return idx;
+    },
+    methods: {
+        addCategory(name) {
+            let newCat = {id: this.nextId++, name: name};
+            this.categories.push(newCat);
+        },
+        _getCategoryIdx(id) {
+            let idx = -1;
+            for (let i = 0; i < this.categories.length; i++) {
+                if (this.categories[i].id === id) {
+                    idx = i;
+                    break;
+                }
+            }
+            return idx;
+        },
+        removeCategory(id) {
+            let idx = this._getCategoryIdx(id)
+            if (idx !== -1) {
+                categories.splice(idx, 1);
+            }
+        },
+        editCategory(id, name) {
+            let idx = this._getCategoryIdx(id);
+            if (idx !== -1) {
+                this.categories[idx].name = name;
+            }
+        }
+    },
+    components: {CategoryManager}
 }
-
-function removeCategory(id) {
-    let idx = _getCategoryIdx(id)
-    if (idx !== -1) {
-        categories.value.splice(idx, 1);
-    }
-}
-
-function editCategory(id, name) {
-    let idx = _getCategoryIdx(id);
-    if (idx !== -1) {
-        categories.value[idx].name = name;
-    }
-}
-
-let nextId = 4;
-
-const categories = ref([
-    { id: 1, name: "One" },
-    { id: 2, name: "Two" },
-    { id: 3, name: "Three" }
-]);
-
 </script>
 
 <template>
