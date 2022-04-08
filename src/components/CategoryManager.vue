b7b90ebeb5221bab31e399a3da2a108367d3d5bc
Krzysztof Cywiński <krzysztof.cywinski@quantumlab.co>
2022-04-08 13:26:35 +0200

    Adding and removing categories to/from EditView data

--- src/components/CategoryManager.vue
+++ src/components/CategoryManager.vue
@@ -1,26 +1,37 @@
 <script setup>
 import IconTrash from '@/components/icons/IconTrash.vue'
 import IconAdd from '@/components/icons/IconAdd.vue'
+import { ref } from 'vue'
+
+const newCategoryInput = ref("")
+
 defineProps({
   categories: {
     type: Array,
-    required: false,
-    default: [
-        { name: "One" },
-        { name: "Two" }
-    ]
+    default: []
   }
 })
+
+function addCategory() {
+    emit('category-add', newCategoryInput.value)
+    newCategoryInput.value = ""
+}
+
+const emit = defineEmits(['category-remove', 'category-add', 'category-edit'])
+
+</script>
+
+<script>
 </script>
 
 <template>
   <div class="categories">
     <h1 class="green">Categories</h1>
     <div v-for="category in categories" :key="category.name">
-        <span>{{ category.name }}</span><IconTrash />
+        <span>{{ category.name }}</span><IconTrash @click="$emit('category-remove', category.id)"/>
     </div>
     <div id="new-category-container">
-        <input type="text" id="new-category-input"/><IconAdd />
+        <input type="text" v-model="newCategoryInput" v-on:keyup.enter="addCategory"/><IconAdd @click="addCategory"/>
     </div>    
   </div>
 </template>
--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,9 +1,41 @@
 <script setup>
-import CategoryManager from '../components/CategoryManager.vue';
+import CategoryManager from '../components/CategoryManager.vue'
+import { ref } from 'vue'
+
+function addCategory(name) {
+    let newCat = {id: nextId++, name: name};
+    categories.value.push(newCat);
+}
+
+function removeCategory(id) {
+    let idx = -1;
+    for (let i = 0; i < categories.value.length; i++) {
+        if (categories.value[i].id === id) {
+            idx = i;
+            break;
+        }
+    }
+    if (idx !== -1) {
+        categories.value.splice(idx, 1);
+    }
+}
+
+let nextId = 4;
+
+const categories = ref([
+    { id: 1, name: "One" },
+    { id: 2, name: "Two" },
+    { id: 3, name: "Three" }
+]);
+
 </script>
 
 <template>
   <main>
-    <CategoryManager />
+    <CategoryManager
+        :categories="categories"
+        @category-add="addCategory"
+        @category-remove="removeCategory"
+    />
   </main>
 </template>
