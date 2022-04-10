f0a4b6d46df2cf443427055fbd7eed3e509db6c8
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2022-04-10 11:31:33 +0200

    icons

---
+++ src/components/icons/IconAdd.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    xmlns:xlink="http://www.w3.org/1999/xlink" 
+    version="1.1" 
+    id="mdi-plus" 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24"
+    class="iconify iconify--mdi"
+    >
+    <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="currentColor"/>
+    </svg>
+</template>
